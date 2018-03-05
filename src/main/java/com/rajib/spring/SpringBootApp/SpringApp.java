c71f5e95a177645c61de85933228c16a10926527
rajib saha <rsaha@nisum.com>
2018-03-04 20:27:24 -0800

    Spring boot app commit

---
+++ src/main/java/com/rajib/spring/SpringBootApp/SpringApp.java
@@ -0,0 +1,12 @@
+package com.rajib.spring.SpringBootApp;
+
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+public class SpringApp {
+	
+	public static void main(String[] args) {  
+	        SpringApplication.run(SpringApp.class, args);  
+	 }  
+}
